79006139fb89a70af81a78c34a1b23f81b9ff648
boroicamarius <boroicamarius01@gmail.com>
2022-05-29 17:43:09 +0300

    first commit

---
+++ bot.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js")
+const open = require('open');
+const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS'] })
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+
+client.on("messageCreate", msg => {
+
+  if(msg.content.toLowerCase() == 'hello')
+    msg.reply('Hello!')
+
+  /* if(msg.author.bot || msg.author.system)
+    return;
+
+    let url;
+    try {
+      url = new URL(msg.content);
+      
+    } catch (_) {
+      return;  
+    }
+
+    msg.react('❤️')
+
+    if(url.hostname == 'youtu.be' || url.host == 'youtube.com')
+        open(url.href) */
+
+})
+
+client.login('TOKEN HERE')
